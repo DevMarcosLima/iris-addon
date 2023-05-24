dff3d7fe060dc7751a7d5a8ebe66c7f3ac3ce957
Marcos <140libra@gmail.com>
2023-05-24 14:05:21 -0300

    ht

--- plugins/cloudrun.py
+++ plugins/cloudrun.py
@@ -101,57 +101,6 @@ class Cloudrun(Plugin):
             return
         try:
             service_name = gcp_object["name"]
-            # {
-            #     'name': 'projects/poc-iris3-exyon/locations/us-central1/services/hello',
-            #     'uid': '5990611e-0f9c-4e8b-a8a1-94e8ac414b5b',
-            #     'generation': '2',
-            #     'labels': {
-            #         'exyon_create': '2023-05-16',
-            #         'exyon_location': 'us-east1',
-            #         'exyon_name': 'hello'
-            #     },
-            #     'createTime': '2023-05-16T19:03:06.150793Z',
-            #     'updateTime': '2023-05-23T20:59:00.124692Z',
-            #     'creator': 'italo.teixeira@ipnet.cloud',
-            #     'lastModifier': 'marcos.lima.t@ipnet.cloud',
-            #     'client': 'cloud-console',
-            #     'ingress': 'INGRESS_TRAFFIC_ALL',
-            #     'launchStage': 'GA',
-            #     'template': {
-            #         'scaling': {'maxInstanceCount': 10},
-            #         'timeout': '300s',
-            #         'serviceAccount': 'poc-iris3-exyon@appspot.gserviceaccount.com',
-            #         'containers': [
-            #             {
-            #                 'image': 'us-docker.pkg.dev/cloudrun/container/hello',
-            #                 'resources': {'limits': {'cpu': '1000m', 'memory': '512Mi'}, 'cpuIdle': True},
-            #                 'ports': [{'name': 'http1', 'containerPort': 8080}],
-            #                 'startupProbe': {'timeoutSeconds': 240, 'periodSeconds': 240, 'failureThreshold': 1, 'tcpSocket': {'port': 8080}}
-            #             }
-            #         ],
-            #         'maxInstanceRequestConcurrency': 80
-            #     },
-            #     'traffic': [
-            #         {'type': 'TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST', 'percent': 100}
-            #     ],
-            #     'observedGeneration': '2',
-            #     'terminalCondition': {
-            #         'type': 'Ready',
-            #         'state': 'CONDITION_SUCCEEDED',
-            #         'lastTransitionTime': '2023-05-23T20:59:15.302196Z'
-            #     },
-            #     'conditions': [
-            #         {'type': 'RoutesReady', 'state': 'CONDITION_SUCCEEDED', 'lastTransitionTime': '2023-05-23T20:59:15.493440Z'},
-            #         {'type': 'ConfigurationsReady', 'state': 'CONDITION_SUCCEEDED', 'lastTransitionTime': '2023-05-23T20:59:09.603445Z'}
-            #     ],
-            #     'latestReadyRevision': 'projects/poc-iris3-exyon/locations/us-central1/services/hello/revisions/hello-00002-4wm',
-            #     'latestCreatedRevision': 'projects/poc-iris3-exyon/locations/us-central1/services/hello/revisions/hello-00002-4wm',
-            #     'trafficStatuses': [
-            #         {'type': 'TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST', 'percent': 100}
-            #     ],
-            #     'uri': 'https://hello-zjb2v377mq-uc.a.run.app',
-            #     'etag': '"CJTStKMGEKDMujs/cHJvamVjdHMvcG9jLWlyaXMzLWV4eW9uL2xvY2F0aW9ucy91cy1jZW50cmFsMS9zZXJ2aWNlcy9oZWxsbw"'
-            # }
 
             # json body
             service_body = gcp_object
@@ -173,7 +122,7 @@ class Cloudrun(Plugin):
             service_body["labels"][f'{prefix}location'] = is_location
 
             self._google_api_client().projects().locations().services().patch(
-                name=f"projects/poc-iris3-exyon/locations/us-central1/services/{is_name}",
+                name=f"projects/{project_id}/locations/{is_location}/services/{is_name}",
                 body=service_body,
             ).execute()
 
