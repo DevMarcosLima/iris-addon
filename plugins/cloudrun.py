dbfa04a918d1309f1ea1e25acd865221282413f4
Marcos <140libra@gmail.com>
2023-05-24 13:59:55 -0300

    ht

--- plugins/cloudrun.py
+++ plugins/cloudrun.py
@@ -162,13 +162,15 @@ class Cloudrun(Plugin):
             is_create = gcp_object["createTime"]
             is_create = is_create.split("T")[0]
             is_location = service_name.split("/")[3]
-
-            service_body = {'name': 'projects/poc-iris3-exyon/locations/us-central1/services/hello', 'uid': '5990611e-0f9c-4e8b-a8a1-94e8ac414b5b', 'generation': '2', 'labels': {'exyon_create':'-', 'exyon_location': '-', 'exyon_name': '-'}, 'createTime': '2023-05-16T19:03:06.150793Z', 'updateTime': '2023-05-23T20:59:00.124692Z', 'creator': 'italo.teixeira@ipnet.cloud', 'lastModifier': 'marcos.lima.t@ipnet.cloud', 
-'client': 'cloud-console', 'ingress': 'INGRESS_TRAFFIC_ALL', 'launchStage': 'GA', 'template': {'scaling': {'maxInstanceCount': 10}, 'timeout': '300s', 'serviceAccount': 'poc-iris3-exyon@appspot.gserviceaccount.com', 'containers': [{'image': 'us-docker.pkg.dev/cloudrun/container/hello', 'resources': {'limits': {'cpu': '1000m', 'memory': '512Mi'}, 'cpuIdle': True}, 'ports': [{'name': 'http1', 'containerPort': 8080}], 'startupProbe': {'timeoutSeconds': 240, 'periodSeconds': 240, 'failureThreshold': 1, 'tcpSocket': {'port': 8080}}}], 'maxInstanceRequestConcurrency': 80}, 'traffic': [{'type': 'TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST', 'percent': 100}], 'observedGeneration': '2', 'terminalCondition': {'type': 'Ready', 'state': 'CONDITION_SUCCEEDED', 'lastTransitionTime': '2023-05-23T20:59:15.302196Z'}, 'conditions': [{'type': 'RoutesReady', 'state': 'CONDITION_SUCCEEDED', 'lastTransitionTime': '2023-05-23T20:59:15.493440Z'}, {'type': 'ConfigurationsReady', 'state': 'CONDITION_SUCCEEDED', 'lastTransitionTime': '2023-05-23T20:59:09.603445Z'}], 'latestReadyRevision': 'projects/poc-iris3-exyon/locations/us-central1/services/hello/revisions/hello-00002-4wm', 'latestCreatedRevision': 'projects/poc-iris3-exyon/locations/us-central1/services/hello/revisions/hello-00002-4wm', 'trafficStatuses': [{'type': 'TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST', 'percent': 100}], 'uri': 'https://hello-zjb2v377mq-uc.a.run.app', 'etag': '"CJTStKMGEKDMujs/cHJvamVjdHMvcG9jLWlyaXMzLWV4eW9uL2xvY2F0aW9ucy91cy1jZW50cmFsMS9zZXJ2aWNlcy9oZWxsbw"'}
             
-            service_body["labels"]['exyon_name'] = is_name
-            service_body["labels"]['exyon_create'] = is_create
-            service_body["labels"]['exyon_location'] = is_location
+            # IF DONT HAVE LABELS
+            if not service_body.get("labels"):
+                service_body["labels"] = {}
+            prefix = "exyon_"
+            # ADD LABELS
+            service_body["labels"][f'{prefix}exyon_name'] = is_name
+            service_body["labels"][f'{prefix}exyon_create'] = is_create
+            service_body["labels"][f'{prefix}exyon_location'] = is_location
 
             self._google_api_client().projects().locations().services().patch(
                 name=f"projects/poc-iris3-exyon/locations/us-central1/services/{is_name}",
